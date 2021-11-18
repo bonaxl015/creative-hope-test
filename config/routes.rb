b770a4ac0ef2f3573f4ef39d485890dc6ea6766d
Bon Axl Feeser <bonaxl015@gmail.com>
2021-11-18 23:06:47 +0800

    Added registration controller to devise and  made the routing for main and home controller easier to expand

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users
+  devise_for :users, controllers: {registrations: "registrations"}
   devise_scope :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
   root to: 'home#index'
+  resources :main, only: [:index]
+  resources :home, only: [:index]
 end
