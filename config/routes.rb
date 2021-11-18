be98578de7dd1a17e2bff810353ca195f6a4f832
Bon Axl Feeser <bonaxl015@gmail.com>
2021-11-18 20:02:39 +0800

    Added route for user sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
-  root to: "home#index"
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+  root to: 'home#index'
 end
